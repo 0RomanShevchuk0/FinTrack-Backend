4b49963f3476ed2db5979043023ffccf8ac1d8dd
RomanShevchuk0 <roman.shevchuk@dmorpheus.design>
2025-03-31 01:34:45 +0200

    auth protection

--- src/currencies/currencies.controller.ts
+++ src/currencies/currencies.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CurrenciesService } from './currencies.service';
 import { CreateCurrencyDto } from './dto/create-currency.dto';
 import { UpdateCurrencyDto } from './dto/update-currency.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @Controller('currencies')
 export class CurrenciesController {
   constructor(private readonly currenciesService: CurrenciesService) {}
-
-  @Post()
-  create(@Body() createCurrencyDto: CreateCurrencyDto) {
-    return this.currenciesService.create(createCurrencyDto);
-  }
 
   @Get()
   findAll() {
@@ -22,12 +26,23 @@ export class CurrenciesController {
     return this.currenciesService.findOne(id);
   }
 
+  @Post()
+  @Auth()
+  create(@Body() createCurrencyDto: CreateCurrencyDto) {
+    return this.currenciesService.create(createCurrencyDto);
+  }
+
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCurrencyDto: UpdateCurrencyDto) {
+  @Auth()
+  update(
+    @Param('id') id: string,
+    @Body() updateCurrencyDto: UpdateCurrencyDto,
+  ) {
     return this.currenciesService.update(id, updateCurrencyDto);
   }
 
   @Delete(':id')
+  @Auth()
   remove(@Param('id') id: string) {
     return this.currenciesService.remove(id);
   }
--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -10,32 +10,38 @@ import {
 import { WalletsService } from './wallets.service';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @Controller('wallets')
 export class WalletsController {
   constructor(private readonly walletsService: WalletsService) {}
 
-  @Post()
-  create(@Body() createWalletDto: CreateWalletDto) {
-    return this.walletsService.create(createWalletDto);
-  }
-
   @Get()
+  @Auth()
   findAll() {
     return this.walletsService.findAll();
   }
 
   @Get(':id')
+  @Auth()
   findOne(@Param('id') id: string) {
     return this.walletsService.findOne(id);
   }
 
+  @Post()
+  @Auth()
+  create(@Body() createWalletDto: CreateWalletDto) {
+    return this.walletsService.create(createWalletDto);
+  }
+
   @Patch(':id')
+  @Auth()
   update(@Param('id') id: string, @Body() updateWalletDto: UpdateWalletDto) {
     return this.walletsService.update(id, updateWalletDto);
   }
 
   @Delete(':id')
+  @Auth()
   remove(@Param('id') id: string) {
     return this.walletsService.remove(id);
   }
